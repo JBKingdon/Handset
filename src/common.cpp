55ff3f037b7b2183127aa60644514cd52d724170
JBKingdon <james.kingdon@gmail.com>
2022-12-05 21:46:38 -0500

    Testing longer preamble for 915
    Modified the 915 125Hz mode to use 16 symbols of preamble instead of 12,
    but the change is commented out for now.

--- src/common.cpp
+++ src/common.cpp
@@ -25,14 +25,22 @@ expresslrs_mod_settings_915_s airRateConfig915 =
 
     // 125Hz
     {0, RATE_125HZ, SX1262_LORA_BW_500, SX1262_LORA_SF6, SX1262_LORA_CR_4_6,  8000, TLM_RATIO_1_8,       4,             12}; // 4896 us @ 8 bytes, 6432 @ 15b, 7200 @ 18b
-                                                                                                          // pdfOffset at 125Hz 2750               1198
+                                                                                                                // pdfOffset at 125Hz 2750               1198
     // measured: 4920 @ 7 bytes, 6520 @ 15 bytes with CR_4_6, preamble 12
     // measured: 4680 @ 7 bytes, 6000 @ 15 bytes with CR_4_5, preamble 10 (turn-around 440us)
+
+    // Testing with longer pre-amble
+    // {0, RATE_125HZ, SX1262_LORA_BW_500, SX1262_LORA_SF6, SX1262_LORA_CR_4_6,  8000, TLM_RATIO_1_8,       4,             16}; //  6944 @ 15b
 
 expresslrs_rf_pref_params_s airRateRFPerf915 =
     //      rate    sens  TOA RFmodeCycleInterval RFmodeCycleAddtionalTime SyncPktIntervalDisconnected SyncPktIntervalConnected pfdOffset   minSNR, maxSNR
     //       83                     NA                  NA                          NA                      NA
     {0, RATE_125HZ, -112,  6432,    3500,               4000,                       200,                   5000,                1185,       0,0};
+
+    // for longer pre-amble, pfdOffset needs validating
+    // {0, RATE_125HZ, -112,  6944,    3500,               4000,                       200,                   5000,                  800,       0,0}; // 673 theory
+
+
 
 #else
 
